5a7dc7ff5544a301751f9b49c1c420d51fa239db
Piotr Schreiner <piotrschreiner@gmail.com>
2023-06-01 02:10:48 +0200

    New Commit

--- src/Feed.js
+++ src/Feed.js
@@ -7,21 +7,24 @@ import EventNoteIcon from "@mui/icons-material/EventNote";
 import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 import InputOption from "./inputOption";
 import Post from "./Post";
-import { db } from "./firebase.js";
+import { db } from "./firebase";
+import firebase from "firebase";
 
 function Feed() {
   const [input, setInput] = useState("");
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection("posts").onSnapshot((snapshot) => {
-      setPosts(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          data: doc.data(),
-        }))
-      );
-    });
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
   }, []);
 
   const sendPost = (e) => {
@@ -30,8 +33,12 @@ function Feed() {
     db.collection("posts").add({
       name: "Pioti HackHack",
       description: "This is a test",
-      //message:
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
+
+    setInput("");
   };
 
   return (
@@ -61,14 +68,16 @@ function Feed() {
           />
         </div>
       </div>
-      {posts.map((post) => (
-        <Post />
+
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
       ))}
-      <Post
-        name="Pioti HackHack"
-        description="This is a test"
-        message="WOW this worked"
-      />
     </div>
   );
 }
